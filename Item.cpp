3fd825e82c1faa7bb1ff07c45623286f1e7aa208
joonseojj <111883022+joonseojj@users.noreply.github.com>
2024-05-09 16:01:49 +0900

    Update Item.cpp
    난수 생성 오류 수정 
    스네이크와 상관 없이 5초마다 위치 갱신

--- Item.cpp
+++ Item.cpp
@@ -1,69 +1,85 @@
-﻿#include <cstdlib>
-#include <ctime>
-#include <chrono> // 시간 측정을 위한 헤더 파일
+#include <cstdlib>
+#include <ctime> //난수 생성용 
+#include <iostream>
+#include <thread>
+#include <chrono> // 시간 계산용 
 #include <curses.h>
 #include "./map.cpp"
-#include "./Snake.cpp"
+
+//int MAP_SIZE = 20;
 
 class Item {
 public:
     int x;
     int y;
-    bool active = false; // 아이템이 활성화되어 있는지
-    std::chrono::steady_clock::time_point lastTime; // 마지막 아이템 초기화시간 저장 
+    bool active = false;
+    std::chrono::steady_clock::time_point lastTime;
 
     Item() {}
-    Item(int _x, int _y) : x(_x), y(_y), active(true) {} // 새로 생성할때는 active를 true로 시작 
+    Item(int _x, int _y) : x(_x), y(_y), active(true) {}
 
-    
-    // 아이템이 스네이크의 머리와 충돌했을 때 
-    // active = false , lastTime = now 하는 메서드
-    virtual void onCollision(Snake& snake) = 0;
-
-    
-    //아이템을 랜덤 위치로 이동시키고, 숨겨져 있는 경우 다시 꺼내는 (생성) 메소드 
     void respawn() { 
-        int new_x = rand() % MAP_SIZE;
-        int new_y = rand() % MAP_SIZE;
-        //스네이크와 닿지 않는 위치로 움직이는 코드 추가
+        int new_x = 9 + std::rand() % MAP_SIZE;
+        int new_y = 9 + std::rand() % MAP_SIZE;
         x = new_x;
         y = new_y;
-        active = true; //활성화
-        lastTime = std::chrono::steady_clock::now(); // 마지막 생성 시간 갱신
+        active = true;
+        lastTime = std::chrono::steady_clock::now();
+    }
+
+    void onCollision()  {
+        active = false;
+        lastTime = std::chrono::steady_clock::now();
     }
 };
 
-class GrothItem : public Item {//상속
+class GrothItem : public Item {
 public:
     GrothItem() {
         respawn();
     }
-
-    void onCollision(Snake& snake)  {
-        snake.increaseLength();// 스네이크 길이 증가
-        active = false;
-        lastTime = std::chrono::steady_clock::now(); // 현재 시간으로 초기화
-    }
-
 };
 
 class PoisonItem : public Item {
-
 public:
     PoisonItem() {
         respawn();
     }
-
-    void onCollision(Snake& snake) {
-        snake.decreaseLength();// 스네이크 길이 감소 
-        active = false;
-        lastTime = std::chrono::steady_clock::now(); // 현재 시간으로 초기화
-    }
-
 };
 
+GrothItem grothItems[3];
+PoisonItem poisonItems[3];
+
+void checkItemCollision() {
+    for (int i = 0; i < 3; ++i) {
+        if (grothItems[i].active &&
+            (std::chrono::steady_clock::now() - grothItems[i].lastTime) >= std::chrono::seconds(5)) {
+            grothItems[i].respawn();
+        }
+
+        if (poisonItems[i].active &&
+            (std::chrono::steady_clock::now() - poisonItems[i].lastTime) >= std::chrono::seconds(5)) {
+            poisonItems[i].respawn();
+        }
+    }
+}
 
 
+/*
+int main() {
+    // 프로그램이 처음 실행될 때만 시드를 설정
+    std::srand(static_cast<unsigned int>(std::time(nullptr))); 
+
+    while (true) {
+        checkItemCollision();
+        for (int i = 0; i < 3; ++i) {
+            std::cout << grothItems[i].x << ' ' << grothItems[i].y << "\n";
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "\n";
+    }
+}
+*/
 
 
 /*
