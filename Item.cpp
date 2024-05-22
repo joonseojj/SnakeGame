46d710a35d674b442851bb9e854261d0c04eafbd
joonseojj <111883022+joonseojj@users.noreply.github.com>
2024-05-23 01:51:32 +0900

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,10 +1,8 @@
-#include <ctime>
+
 #include <cstdlib>
-#include <ctime>
 #include <chrono>
 #include <thread>
 #include <curses.h>
-#include "map.cpp"
 
 class Item {
 public:
@@ -18,15 +16,6 @@ public:
         y = 0;
         active = false;
     }  
-
-    void respawn() {
-        int new_x = 9 + std::rand() % MAP_SIZE;
-        int new_y = 9 + std::rand() % MAP_SIZE;
-        x = new_x;
-        y = new_y;
-        active = true;
-        lastTime = std::chrono::steady_clock::now();
-    }
 
     void onCollision() {
         active = false;
@@ -44,39 +33,3 @@ public:
     PoisonItem() : Item() { }
 };
 
-
-
-//
-//void checkItemTime(GrowthItem growthItems[], PoisonItem poisonItems[]) {
-//    for (int i = 0; i < 3; ++i) {
-//        if (growthItems[i].active &&
-//            (std::chrono::steady_clock::now() - growthItems[i].lastTime) >= std::chrono::seconds(5)) {
-//            growthItems[i].respawn();
-//        }
-//
-//        if (poisonItems[i].active &&
-//            (std::chrono::steady_clock::now() - poisonItems[i].lastTime) >= std::chrono::seconds(5)) {
-//            poisonItems[i].respawn();
-//        }
-//    }
-//}
-//
-///*
-//void checkItemCollision(GrowthItem growthItems[], PoisonItem poisonItems[], Snake& snake) {
-//    for (int i = 0; i < 3; ++i) {
-//        if (growthItems[i].active &&
-//            growthItems[i].x == snake.getPositionOf(0)[0] &&
-//            growthItems[i].y == snake.getPositionOf(0)[1]) {
-//            growthItems[i].onCollision();
-//            snake.increaseLength();
-//        }
-//
-//        if (poisonItems[i].active &&
-//            poisonItems[i].x == snake.getPositionOf(0)[0] &&
-//            poisonItems[i].y == snake.getPositionOf(0)[1]) {
-//            poisonItems[i].onCollision();
-//            snake.decreaseLength();
-//        }
-//    }
-//}
-//*/
